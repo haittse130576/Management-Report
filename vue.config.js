efba6d4bed6760589ab8f43bd7c214a68de9e4ab
ducmanh1666 <manhlvdse130605@fpt.edu.vn>
2022-03-31 22:25:20 +0700

    remote change to localhost

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,8 @@ module.exports = {
   devServer: {
     headers: { 'Access-Control-Allow-Origin': '*' },
     proxy: {
-      '/api': {
-        target: 'https://localhost:7250/',
+      "/api": {
+        target: 'https://localhost:7250',
         changeOrigin: true,
         secure: false,
       },
